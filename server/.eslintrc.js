49af2dc7025e68c151801fceac485b9ef5c57162
gopaladhikari <gopuadks@gmail.com>
2024-03-19 12:51:23 +0545

    updated eslint in server

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,6 +1,6 @@
-export default {
+module.exports = {
   env: {
-    node: true,
+    browser: true,
     es2021: true,
   },
   extends: [
@@ -21,9 +21,13 @@ export default {
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2021,
     sourceType: "module",
   },
   plugins: ["@typescript-eslint"],
-  rules: {},
+  rules: {
+    "node/no-unsupported-features/es-syntax": 0,
+    "node/no-missing-import": 0,
+    "node/file-extension-in-import": 0,
+  },
 };
--- server/src/controllers/user.controller.ts
+++
@@ -1,4 +0,0 @@
-const options = {
-  httpOnly: true,
-  secure: true,
-};
