0ce125c7bd29a689fcf96aae1535cfe3df2c15c6
gopaladhikari <gopuadks@gmail.com>
2024-03-21 16:44:36 +0545

    restored cloudinary

---
+++ server/src/utils/cloudinary.ts
@@ -0,0 +1,21 @@
+import { v2 as cloudinary } from "cloudinary";
+import { env } from "../conf/env";
+import fs from "fs";
+
+cloudinary.config({
+	cloud_name: env.cloudinaryName,
+	api_key: env.cloudinaryApiKey,
+	api_secret: env.cloudinarySecret,
+});
+
+export const uploadOnCloudinary = async (filePath: string) => {
+	try {
+		const result = await cloudinary.uploader.upload(filePath, {
+			resource_type: "auto",
+		});
+		fs.unlinkSync(filePath);
+		return result.url;
+	} catch (error) {
+		fs.unlinkSync(filePath);
+	}
+};
