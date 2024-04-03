9931af56ee13a3006458bd9994bba515a11e1def
gopuadks <gopuadks@gmail.com>
2024-04-03 20:36:02 +0545

    added getMovieById router and controller

--- server/src/controllers/movie.controller.ts
+++ server/src/controllers/movie.controller.ts
@@ -1,3 +1,4 @@
+import { isValidObjectId } from "mongoose";
 import { Movie } from "../models/movie.model";
 import { ApiError } from "../utils/ApiError";
 import { ApiResponse } from "../utils/ApiResponse";
@@ -29,7 +30,20 @@ const getAllMovies = dbHandler(async (req, res) => {
   if (!movies?.length)
     return res.status(404).json(new ApiError(404, "Movies not found"));
 
-  res.status(200).json(new ApiResponse(200, { movies }, "Movies fetched"));
+  res.status(200).json(new ApiResponse(200, movies, "Movies fetched"));
 });
 
-export { getAllMovies };
+const getMovieById = dbHandler(async (req, res) => {
+  const { id } = req.params;
+
+  if (!isValidObjectId(id))
+    return res.status(400).json(new ApiError(400, "Invalid params"));
+
+  const movie = await Movie.findById(id);
+
+  if (!movie) return res.status(404).json(new ApiError(404, "Movie not found"));
+
+  res.status(200).json(new ApiResponse(200, movie, "Movie fetched"));
+});
+
+export { getAllMovies, getMovieById };
--- server/src/routes/movie.route.ts
+++ server/src/routes/movie.route.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getAllMovies } from "../controllers/movie.controller";
+import { getAllMovies, getMovieById } from "../controllers/movie.controller";
 
 const movieRouter = Router();
 
 movieRouter.route("/get-all-movies").get(getAllMovies);
+movieRouter.route("/get-movie-by-id/:id").get(getMovieById);
 
 export { movieRouter };
