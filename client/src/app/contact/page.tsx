7298c3c27de05ab44add79f5aa4d678e99eaa68e
gopaladhikari <gopuadks@gmail.com>
2024-04-23 11:36:32 +0545

    embedeed google map

--- client/src/app/contact/page.tsx
+++ client/src/app/contact/page.tsx
@@ -60,6 +60,20 @@ export default function page() {
 					</aside>
 				</MaxwidthWrapper>
 			</section>
+
+			<section>
+				<MaxwidthWrapper>
+					<iframe
+						title="GoogleMap"
+						src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d57148.70073092478!2d87.46203900462739!3d26.462374980878973!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ef7e58a2911917%3A0x245693bf47a14e6!2sRangeli%2056600!5e0!3m2!1sen!2snp!4v1713850612700!5m2!1sen!2snp"
+						height={450}
+						allowFullScreen
+						loading="lazy"
+						referrerPolicy="no-referrer-when-downgrade"
+						className="my-16 w-full"
+					/>
+				</MaxwidthWrapper>
+			</section>
 		</main>
 	);
 }
