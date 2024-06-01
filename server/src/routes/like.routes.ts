ecddb2a8456ab1642bddf383bbf3d82533737573
gopaladhikari <gopuadks@gmail.com>
2024-06-01 13:52:58 +0545

    updated like route

--- server/src/routes/like.routes.ts
+++ server/src/routes/like.routes.ts
@@ -5,7 +5,7 @@ import { toggleLike } from "../controllers/like.controller";
 const likeRouter = Router();
 
 likeRouter
-  .route("/toggle-like")
+  .route("/toggle-like/:movieId")
   .post(passport.authenticate("jwt", { session: false }), toggleLike);
 
 export { likeRouter };
